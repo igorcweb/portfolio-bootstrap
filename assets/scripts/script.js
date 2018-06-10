4a9b7ed4e6a904f6664e66fd2e8add91b8d08c3e
Igor Chernyshev <igorchern07@gmail.com>
2018-06-10 17:27:38 -0500

    finsihed responsive portfolio

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,10 +1,4 @@
 (function() {
   var year = document.querySelector('#year');
   year.innerText = new Date().getFullYear();
-  document.querySelector('button').addEventListener('mouseover', function() {
-    this.classList.add('btn-outline-till');
-  });
-  document.querySelector('button').addEventListener('mouseout', function() {
-    this.classList.remove('btn-outline-till');
-  });
 })();
